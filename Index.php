98196af6f8dfc5160836b37eca0b51019f501d63
Bencio09 <lorebencini13@outlook.com>
2022-04-08 17:27:39 +0200

    znm,drtfgb€AFRBG-,NMADFH,G

---
+++ Index.php
@@ -0,0 +1,21 @@
+<html>
+
+<head>
+    <title>Analisi Statistica Testo</title>
+    <link rel="stylesheet" href="./CSS/bootstrap.css" type="text/css">
+    <script src="./JS/bootstrap.js"></script>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col"></div>
+            <div class="col">
+                <h1 class="alert alert-primary" style="border-radius: 5px; text-align: center;">ANALISI STATISTICA</h1>
+            </div>
+            <div class="col"></div>
+        </div>
+    </div>
+</body>
+
+</html>
